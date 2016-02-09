8de85cbdfc864151ec0e1c5637fcbec34a654dd7
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-10 03:57:11 +0800

    Created dashboard on views

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MarkWayne
- * Date: 2/9/2016
- * Time: 4:37 PM
- */
+@extends('layouts.admin')
+
+@section('contents')
+ Hello!
+@stop
+
+
+@section('footer')
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>Version</b> 2.3.0
+        </div>
+        <strong>Copyright &copy</strong>
+    </footer>
+
+@stop
