9b05ed8bee09c5f284ba58302d4b0bf21183855d
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-10 04:01:04 +0800

    Created UserGroup models with properties

---
+++ app/Models/UserGroup.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserGroup extends Model
+{
+    protected $table = 'user_groups';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function users()
+    {
+        return $this->hasMany('App\Models\User');
+    }
+}
