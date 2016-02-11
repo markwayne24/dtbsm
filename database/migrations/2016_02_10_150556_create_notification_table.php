2588a05bfefe3519a742c5a8e85a3dbb42f1421a
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-11 14:24:41 +0800

    created notification table on database migration

---
+++ database/migrations/2016_02_10_150556_create_notification_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notif', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->unsigned();
+            $table->string('name');
+            $table->string('description');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notif');
+    }
+}
