c70269e2ba753a38b1b7a57c4b76b08ea73a8726
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-12 08:03:08 +0800

    Changed redirect path on AuthControllers

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/dashboard';
+    protected $redirectTo = '/login/verify';
 
     /**
      * Create a new authentication controller instance.
