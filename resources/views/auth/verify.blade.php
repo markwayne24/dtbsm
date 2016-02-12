a89e306238c155966f7719dca9dcf7dafae1079d
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-12 08:08:01 +0800

    added verification page on views

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,7 +1,42 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MarkWayne
- * Date: 2/11/2016
- * Time: 3:10 PM
- */
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Hi, {{ Auth::user()->firstname }} .Please enter your security code.</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('login/verify') }}">
+                            {!! csrf_field() !!}
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Enter Code</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="code">
+
+                                    @if ($errors->has('code'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('code') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-btn fa-security"></i>Verify
+                                    </button>
+
+                                    <a class="btn btn-link" href="{{ url('#') }}">Regenerate code?</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
