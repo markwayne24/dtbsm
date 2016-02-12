01954ee968788db704c640ef5f6a3de41459a86d
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-12 08:09:40 +0800

    Edited Authentication links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,17 +28,6 @@
 <body id="app-layout">
     <nav class="navbar navbar-default">
         <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-            </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
 
@@ -46,16 +35,11 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->firstname }} <span class="caret"></span>
+                            <a href="{{ url('/logout') }}" role="button" aria-expanded="false">
+                                <strong>Login</strong>
                             </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
                         </li>
                     @endif
                 </ul>
