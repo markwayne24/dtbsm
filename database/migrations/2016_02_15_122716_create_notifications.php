bb4b376950c0e8c2755d2eb109180027b2b9fb8d
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-16 07:24:55 +0800

    Created notifications on migrations

---
+++ database/migrations/2016_02_15_122716_create_notifications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('is_global');
+            $table->integer('from_user_id');
+            $table->integer('to_user_id');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notifications');
+    }
+}
