7fe34cb07b8fe3eaf1974ecacdb147c46ac52211
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-16 07:20:31 +0800

    Created requests on migration

---
+++ database/migrations/2016_02_15_122202_create_requests.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('requests');
+    }
+}
