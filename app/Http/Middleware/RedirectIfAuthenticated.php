26ceb620c4781a2bd65246117a6d0290a00d4d39
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-16 07:00:48 +0800

    Added comment on middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use app\Models\User;
 
 class RedirectIfAuthenticated
 {
@@ -18,7 +19,13 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/admin/dashboard');
+/*            $users = User::with('userGroup')->get();
+            if ($users->userGroup->name == 'admin'){*/
+                return redirect('/admin/dashboard');
+/*            }else{
+                return redirect('');
+            }*/
+
         }
 
         return $next($request);
