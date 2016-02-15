9979ba6cc725ace2454e58fb212d7d09b13b15d2
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-16 07:23:45 +0800

    Created item type on migrations

---
+++ database/migrations/2016_02_15_122656_create_item_type.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_type', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('item_type');
+    }
+}
