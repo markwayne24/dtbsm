ec202e54daafca040f782e0f06d736038ac45b5b
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-16 07:22:47 +0800

    Created inventory on migration

---
+++ database/migrations/2016_02_15_122619_create_inventory.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->string('sku');
+            $table->double('price');
+            $table->integer('stocks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('inventory');
+    }
+}
