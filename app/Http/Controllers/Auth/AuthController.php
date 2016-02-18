f368adaa7912c961a021d43d3784f18a2ffae9c1
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-19 06:36:00 +0800

    Removed RedirectAfterlogout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
      * @var string
      */
     protected $redirectTo = '/login/verify';
-    //protected $redirectAfterLogout = '/linkToRedirect';
+/*    protected $redirectAfterLogout = '/logout';*/
 
     /**
      * Create a new authentication controller instance.
