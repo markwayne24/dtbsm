e28a12ab6dd2573976ff8e29881f4cd1ca74a80b
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-19 07:11:05 +0800

    Added user's page for users on users views

--- resources/views/users/index.php
+++ resources/views/users/index.php
@@ -1,7 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MarkWayne
- * Date: 2/9/2016
- * Time: 5:24 PM
- */
+@extends('layouts.user')
+
+@section('content')
+    Welcome home {{ Auth::user()->userProfile->firstname }}
+@stop
