9076fc955fc2d596b4cac25f05e19fcd1d4b0646
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-27 09:05:41 +0800

    Added users views

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,77 +1,126 @@
 @extends('layouts.admin')
 
 @section('content')
-        <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            User's Page
-        </h1>
-    </section>
-    <section class="content-header">
-    </section>
-
-    <div class="container">
-        <section>
-            <div class="row">
-                <!-- left column -->
-                <div class="col-md-7">
-                    <div class="box box-primary">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">List of Users</h3>
-                        </div><!-- /.box-header -->
-                        <div class="box-body">
-
-                            <table class="table table-bordered">
-                                <tr>
-                                    <th style="width: 10px"></th>
-                                    <th style="width: 10px">Name</th>
-                                    <th style="width: 40px">Role</th>
-                                    <th style="width: 10px">Actions</th>
-                                </tr>
-
-                                @foreach ($users as $user)
-
-                                    <tr>
-                                        <td>{{ $user->id }}</td>
-                                        <td>{{ $user->userProfile->firstname }}</td>
-                                        <td>{{$user->userGroup->name}}</td>
-                                        <td>
-                                            <button class="btn btn-success btn-flat"><a href="#"><i class="fa fa-pencil-square-o"></i></a></button>
-                                            <button class="btn btn-info btn-flat"><a href="#"><i class="fa fa-pencil-square-o"></i></a></button>
-                                            <button class="btn btn-danger btn-flat"><a href="#"><i class="fa fa-trash-o"></i></a></button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </table>
-                        </div><!-- /.box-body -->
-                        <div class="box-footer clearfix">
-                            <ul class="pagination pagination-sm no-margin pull-right">
-                                <li>{!! $users->render() !!}</li>
-                            </ul>
-                        </div>
-                    </div><!-- /.box -->
-                </div>
-
-                <!-- right column -->
-                <div class="col-md-4">
-                    <!-- general form elements -->
-                    <div class="box box-primary">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Registration Form</h3>
-                        </div><!-- /.box-header -->
-                        <!-- form start -->
-                        @if (Session::has('message'))
-                            <div class="alert alert-info">{{ Session::get('message') }}</div>
-                        @endif
-
-                        @include('admin.users.form');
-                    </div><!-- /.box -->
-                </div> <!-- end of right pane-->
-            </div>
-        </section>
-    </div>
-</div>
+ @include('admin.users.lists')
 @stop
+
+@section('scripts')
+    <script>
+    $(function () {
+        $("#example1").DataTable();
+    });
+    </script>
+
+    <script>
+        $(function () {
+            //Initialize Select2 Elements
+            $(".select2").select2();
+        });
+    </script>
+    <script>
+        $('document').ready(function() {
+
+            // Prepare reset.
+            function resetModalFormErrors() {
+                $('.form-group').removeClass('has-error');
+                $('.form-group').find('.help-block').remove();
+            }
+
+            // Intercept submit.
+            $('form.bootstrap-modal-form').on('submit', function(submission) {
+                submission.preventDefault();
+
+                // Set vars.
+                var form   = $(this),
+                        url    = form.attr('action'),
+                        submit = form.find('[type=submit]');
+
+                // Check for file inputs.
+                if (form.find('[type=file]').length) {
+
+                    // If found, prepare submission via FormData object.
+                    var input       = form.serializeArray(),
+                            data        = new FormData(),
+                            contentType = false;
+
+                    // Append input to FormData object.
+                    $.each(input, function(index, input) {
+                        data.append(input.name, input.value);
+                    });
+
+                    // Append files to FormData object.
+                    $.each(form.find('[type=file]'), function(index, input) {
+                        if (input.files.length == 1) {
+                            data.append(input.name, input.files[0]);
+                        } else if (input.files.length > 1) {
+                            data.append(input.name, input.files);
+                        }
+                    });
+                }
+
+                // If no file input found, do not use FormData object (better browser compatibility).
+                else {
+                    var data        = form.serialize(),
+                            contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
+                }
+
+                // Please wait.
+                if (submit.is('button')) {
+                    var submitOriginal = submit.html();
+                    submit.html('Please wait...');
+                } else if (submit.is('input')) {
+                    var submitOriginal = submit.val();
+                    submit.val('Please wait...');
+                }
+
+                // Request.
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: data,
+                    dataType: 'json',
+                    cache: false,
+                    contentType: contentType,
+                    processData: false
+
+                    // Response.
+                }).always(function(response, status) {
+
+                    // Reset errors.
+                    resetModalFormErrors();
+
+                    // Check for errors.
+                    if (response.status == 422) {
+                        var errors = $.parseJSON(response.responseText);
+
+                        // Iterate through errors object.
+                        $.each(errors, function(field, message) {
+                            console.error(field+': '+message);
+                            var formGroup = $('[name='+field+']', form).closest('.form-group');
+                            formGroup.addClass('has-error').append('<p class="help-block">'+message+'</p>');
+                        });
+
+                        // Reset submit.
+                        if (submit.is('button')) {
+                            submit.html(submitOriginal);
+                        } else if (submit.is('input')) {
+                            submit.val(submitOriginal);
+                        }
+
+                        // If successful, reload.
+                    } else {
+                        location.reload();
+                    }
+                });
+            });
+
+            // Reset errors when opening modal.
+            $('.bootstrap-modal-form-open').click(function() {
+                resetModalFormErrors();
+            });
+
+        });
+    </script>
+@stop
+
 
