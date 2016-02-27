cc8da29ae46e66e7ca28a6e920fd3c07e3a8154b
Mark Wayne Pulido <Mark Wayne Pulido>
2016-02-27 08:56:34 +0800

    Added items routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,27 +29,44 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/', 'Auth\AuthController@showLoginForm');
     Route::get('/login', 'Auth\AuthController@showLoginForm');
     Route::post('/login', 'Auth\AuthController@login');
+    Route::get('logout', 'Auth\AuthController@logout');
 
+    Route::group(['middleware'=>'verify'], function(){
 
         Route::get('/login/verify','Auth\VerifyController@index');
         Route::post('/login/verify','Auth\VerifyController@verify');
 
-        Route::get('logout', 'Auth\AuthController@logout');
-
         //Admin dashboard if verified
         Route::group(['prefix' => 'admin/dashboard','middleware'=>'admin'], function(){
             //admin dashboard
-            Route::get('/','Dashboard\DashboardController@index');
+            Route::get('/','Admin\Dashboard\DashboardController@index');
             //User's list and Registration
             Route::group(['prefix' => 'users'], function() {
-                Route::get('/', 'User\UsersController@index');
-                Route::post('/', 'User\UsersController@store');
-                Route::post('/{userId}','User\UsersController@update');
-                Route::post('/{userId}/delete','User\UsersController@destroy');
+                Route::get('/', 'Admin\User\UsersController@index');
+                Route::post('/', 'Admin\User\UsersController@store');
+                Route::post('/{userId}','Admin\User\UsersController@update');
+                Route::post('/{userId}/delete','Admin\User\UsersController@destroy');
+            });
+
+            //Items
+            Route::group(['prefix' => 'supplies/items'], function() {
+                Route::get('/', 'Admin\Item\ItemsController@index');
+                Route::post('/', 'Admin\Item\ItemsController@store');
+                Route::post('/{userId}','Admin\Item\ItemsController@update');
+                Route::post('/{userId}/delete','Admin\Item\ItemsController@destroy');
+            });
+
+            //Items types
+            Route::group(['prefix' => 'supplies/item-types'], function() {
+                Route::get('/', 'Admin\ItemType\ItemTypesController@index');
+                Route::post('/', 'Admin\ItemType\ItemTypesController@store');
+                Route::post('/{userId}','Admin\ItemType\ItemTypesController@update');
+                Route::post('/{userId}/delete','Admin\ItemType\ItemTypesController@destroy');
             });
         });
         //User's page
         Route::group(['prefix' => '/user'], function(){
             return "User's page";
+        });
     });
 });
