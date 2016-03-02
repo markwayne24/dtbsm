5ab1b67876ff967a283e4741bb022fcf4b22ec74
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-02 16:24:21 +0800

    Added ItemType on Models

---
+++ app/Models/ItemType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemType extends Model
+{
+    protected $table = 'item_type';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function items(){
+        return $this->hasMany('App\Models\Items');
+    }
+}
