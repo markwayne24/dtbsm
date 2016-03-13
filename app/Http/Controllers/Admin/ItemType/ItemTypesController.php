0ec50014be14b980c28288b47e2569a7b1ad9d07
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-13 15:19:07 +0800

    Updated ItemTypesController on controller

--- app/Http/Controllers/Admin/ItemType/ItemTypesController.php
+++ app/Http/Controllers/Admin/ItemType/ItemTypesController.php
@@ -26,9 +26,33 @@ class ItemTypesController extends Controller
     }
 
     public function store(StoreItemsRequest $request){
-        $items = $request->all();
-        ItemType::create($items);
+        $input = $request->all();
+        $item_type = ItemType::create($input);
+
+        return response()->json($item_type);
+
         \Session::flash('flash_message','Successfully Saved.');
         return redirect()->back()->with('message',flash_message);
+    }
+
+    public function edit($item_types)
+    {
+        $items = ItemType::findOrFail($item_types);
+
+        return response()->json($items);
+    }
+
+    public function update(StoreItemsRequest $request, $item_types)
+    {
+        $input = $request->all();
+        $item_type = ItemType::where('id', $item_types)->update($input);
+
+        return response()->json($item_type);
+    }
+
+    public function destroy($item_types)
+    {
+        $item_type = ItemType::destroy($item_types);
+        return response()->json($item_type);
     }
 }
