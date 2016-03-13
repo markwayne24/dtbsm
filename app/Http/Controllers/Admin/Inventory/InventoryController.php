b73b51d6d1e9ee7c6aa816a3ed058cc076850ccb
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-13 15:16:50 +0800

    Added inventory Controller

--- app/Http/Controllers/Admin/Inventory/InventoryController.php
+++ app/Http/Controllers/Admin/Inventory/InventoryController.php
@@ -2,11 +2,50 @@
 
 namespace App\Http\Controllers\Admin\Inventory;
 
+use App\Http\Requests\StoreInventoryRequest;
+use App\Models\Items;
 use Illuminate\Session\SessionManager;
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Inventory;
 
 class InventoryController extends Controller
 {
+    private $session;
+
+    public function __construct(SessionManager $session)
+    {
+        $this->session = $session;
+    }
+
+    public function index()
+    {
+        $inventories = Inventory::with('items')->get();
+        $items = Items::with('itemTypes')->get();
+        return view('admin.items.inventory.index', compact('inventories','items'));
+    }
+
+    public function store(StoreInventoryRequest $request)
+    {
+        $id = $request->only('item_id');
+
+        $id = Items::where('name',$id)->first();
+        $type = $id->id;
+        $items = Inventory::create($request->all());
+        $items->item_id = $type;
+        $items->save();
+
+        \Session::flash('flash_message','Successfully created Inventory.');
+        return redirect()->back()->with('message',flash_message);
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 
 }
