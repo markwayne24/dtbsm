292f1525b19a92daa2d655a1b635927862229c7a
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-13 23:05:45 +0800

    Added validation if has data on  itemTypes views

--- resources/views/admin/items/itemTypes/index.blade.php
+++ resources/views/admin/items/itemTypes/index.blade.php
@@ -243,6 +243,7 @@
                     },
 
                     success: function() {
+                        $('#confirmBox').modal('hide')
                         parent.fadeOut(300,function() {
                             parent.remove();
                         });
@@ -256,78 +257,6 @@
             $('.deleteModal').click(function() {
                 var id = $(this).val();
                 $('#deleteItem').val(id);
-            });
-        });
-    </script>
-
-    <script>
-        $(document).ready(function() {
-
-            // We have two control functions that show or hide dialogs
-
-            function showDialog(id){
-
-                // Find the dialog and show it
-
-                var dialog = $('#' + id),
-                        card = dialog.find('.dialog-card');
-
-                dialog.fadeIn();
-
-                // Center it on screen
-
-                card.css({
-                    'margin-top' : -card.outerHeight()/2
-                });
-
-            }
-
-            function hideAllDialogs(){
-
-                // Hide all visible dialogs
-                $('.dialog-overlay').fadeOut();
-
-            }
-
-            // Here is how to use these functions
-
-            $('.dialog-confirm-button, .dialog-reject-button').on('click', function () {
-
-                // Hide the dialog when the confirm button is pressed
-                hideAllDialogs();
-
-            });
-
-            $('.dialog-overlay').on('click', function (e) {
-
-                if(e.target == this){
-                    // If the overlay was clicked/touched directly, hide the dialog
-                    hideAllDialogs();
-                }
-
-
-            });
-
-            $(document).keyup(function(e) {
-
-                if (e.keyCode == 27) {
-                    // When escape is pressed, hide all dialogs
-
-                    hideAllDialogs();
-                }
-
-            });
-
-
-            // Here, we are listening for clicks on the "show dialog" buttons,
-            // and showing the correct dialog
-
-            $('.dialog-show-button').on('click', function () {
-
-                // Take the contents of the  "data-show-dialog" attribute
-                var toShow = $(this).data('show-dialog');
-
-                showDialog(toShow);
             });
         });
     </script>
