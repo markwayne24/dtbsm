d37c9676d82315ff6346665034e55654bef75f2c
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-13 15:23:50 +0800

    Updated User groups on UserGroupsTableSeeder

--- database/seeds/UserGroupsTableSeeder.php
+++ database/seeds/UserGroupsTableSeeder.php
@@ -24,7 +24,7 @@ class UserGroupsTableSeeder extends Seeder
         ];
 
         foreach ($userGroups as $group) {
-            \App\Models\UserGroup::create($group);
+            UserGroup::create($group);
         }
     }
 }
