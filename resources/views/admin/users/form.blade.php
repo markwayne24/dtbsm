f12543819a3e33e3580ead5d2ea5c0413eb5b4df
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-13 15:29:04 +0800

    Updated form on user views

--- resources/views/admin/users/form.blade.php
+++ resources/views/admin/users/form.blade.php
@@ -2,7 +2,7 @@
 <div class="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <form role="form" method="POST" action="users" class="bootstrap-modal-form">
+            <form role="form" class="bootstrap-modal-form">
                 <div class="modal-header bg-primary">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title" id="myModalLabel">Create Users</h4>
@@ -12,9 +12,9 @@
                     <div class="form-group{{ $errors->has('group_id') ? ' has-error' : '' }}">
                         <!-- Select2-->
                         <label>Item Type</label>
-                        <select class="form-control select2" style="width: 100%;" name="group_id">
+                        <select class="form-control select2" style="width: 100%;" name="group_id" id="group_id">
                             @foreach ($usertypes as $usertype)
-                                <option>{{ $usertype->name }}</option>
+                                <option id="{{$usertype->id}}">{{ $usertype->name }}</option>
                             @endforeach
                         </select>
                         @if ($errors->has('group_id'))
@@ -27,7 +27,7 @@
                         <div class="col-lg-4">
                         <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
                             <label for="exampleInputPassword1">First Name</label>
-                            <input type="text" class="form-control" placeholder="Enter Firstname" name="firstname">
+                            <input type="text" class="form-control" placeholder="Enter Firstname" name="firstname" id="firstname">
                             @if ($errors->has('firstname'))
                                 <span class="help-block">
                                      <strong>{{ $errors->first('firstname') }}</strong>
@@ -38,7 +38,7 @@
                         <div class="col-lg-4">
                         <div class="form-group{{ $errors->has('middlename') ? ' has-error' : '' }}">
                             <label for="exampleInputPassword1">Middle Name</label>
-                            <input type="text" class="form-control" placeholder="Enter Middlename" name="middlename">
+                            <input type="text" class="form-control" placeholder="Enter Middlename" name="middlename" id="middlename">
                             @if ($errors->has('middlename'))
                                 <span class="help-block">
                                      <strong>{{ $errors->first('middlename') }}</strong>
@@ -49,7 +49,7 @@
                         <div class="col-lg-4">
                         <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
                             <label for="exampleInputPassword1">Last Name</label>
-                            <input type="text" class="form-control" placeholder="Enter Lastname" name="lastname">
+                            <input type="text" class="form-control" placeholder="Enter Lastname" name="lastname" id="lastname">
 
                             @if ($errors->has('lastname'))
                                 <span class="help-block">
@@ -61,7 +61,7 @@
                     </div><!--end of the row-->
                     <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
                         <label for="exampleInputEmail1">Address</label>
-                        <input type="text" class="form-control" placeholder="Enter Address" name="address">
+                        <input type="text" class="form-control" placeholder="Enter Address" name="address" id="address">
 
                         @if ($errors->has('address'))
                             <span class="help-block">
@@ -91,7 +91,7 @@
                         <div class="col-lg-6">
                             <div class="form-group{{ $errors->has('contact_number') ? ' has-error' : '' }}">
                                 <label for="exampleInputPassword1">Contact Number</label>
-                                <input type="text" class="form-control" placeholder="Enter Contact No." name="contact_number">
+                                <input type="text" class="form-control" placeholder="Enter Contact No." name="contact_number" id="contact_number">
 
                                 @if ($errors->has('contact_number'))
                                     <span class="help-block">
@@ -104,7 +104,7 @@
 
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">Email</label>
-                        <input type="email" class="form-control" placeholder="Enter Email" name="email">
+                        <input type="email" class="form-control" placeholder="Enter Email" name="email" id="email">
 
                         @if ($errors->has('email'))
                             <span class="help-block">
@@ -116,7 +116,7 @@
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">Password</label>
 
-                        <input type="password" class="form-control" placeholder="Enter Password" name="password">
+                        <input type="password" class="form-control" placeholder="Enter Password" name="password" id="password">
 
                         @if ($errors->has('password'))
                             <span class="help-block">
@@ -139,7 +139,8 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Create</button>
+                    <button type="button" class="btn btn-primary btn-save">Create</button>
+                    <input type="hidden" id="user_id" name="user_id" value="0">
                 </div>
             </form>
         </div>
