7ba781983737c40c58999ed0d2e6020004d2ab52
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-19 10:11:03 +0800

    Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,35 +40,15 @@ Route::group(['middleware' => 'web'], function () {
         Route::group(['prefix' => 'admin/dashboard','middleware'=>'admin'], function(){
             //admin dashboard
             Route::get('/','Admin\Dashboard\DashboardController@index');
+
             //User's list and Registration
-/*            Route::resource('users','Admin\User\UsersController');*/
             Route::resource('users','Admin\User\UsersController');
-  /*          Route::group(['prefix' => 'users'], function() {
-              Route::get('/', 'Admin\User\UsersController@index');
-                Route::post('/', 'Admin\User\UsersController@store');
-                Route::get('/{userId}/edit', 'Admin\User\UsersController@edit');
-                Route::post('/{userId}','Admin\User\UsersController@update');
-                Route::post('/{userId}/delete','Admin\User\UsersController@destroy');
-            });*/
 
             //Items
             Route::resource('supplies/items','Admin\Item\ItemsController');
-/*            Route::group(['prefix' => 'supplies/items'], function() {
-                Route::get('/', 'Admin\Item\ItemsController@index');
-                Route::post('/', 'Admin\Item\ItemsController@store');
-                Route::get('/{itemId}', 'Admin\Item\ItemsController@edit');
-                Route::post('/{itemId}','Admin\Item\ItemsController@update');
-                Route::delete('/{itemId}','Admin\Item\ItemsController@destroy');
-            });*/
 
             //Items types
             Route::resource('supplies/item-types','Admin\ItemType\ItemTypesController');
-/*            Route::group(['prefix' => 'supplies/item-types'], function() {
-                Route::get('/', 'Admin\ItemType\ItemTypesController@index');
-                Route::post('/', 'Admin\ItemType\ItemTypesController@store');
-                Route::post('/{item-typesId}','Admin\ItemType\ItemTypesController@update');
-                Route::post('/{item-typesId}/delete','Admin\ItemType\ItemTypesController@destroy');
-            });*/
 
             //Inventory
             Route::resource('supplies/inventory','Admin\Inventory\InventoryController');
@@ -81,8 +61,6 @@ Route::group(['middleware' => 'web'], function () {
         });
 
         //User's page
-        Route::group(['prefix' => '/user'], function(){
-            return "User's page";
-        });
+        Route::resource('users','User\Profile\ProfileController');
     });
 });
