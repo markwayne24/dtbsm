d2f9ffdef2d54a73b0cd44e258aa946667edddbb
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-19 10:20:15 +0800

    Updated lists on users view

--- resources/views/admin/users/lists.blade.php
+++ resources/views/admin/users/lists.blade.php
@@ -14,6 +14,7 @@
     </section>
     <section>
         @include('admin.users.form')
+        @include('admin.users.info');
     </section>
 
     <!-- Main content -->
@@ -46,41 +47,43 @@
                             </thead>
                             <tbody>
                             @if($users)
-                            @foreach($users as $user)
-                                <tr id="user-{{$type->id}}">
-                                    <td>{{$user->id}}</td>
-                                    <td>{{$user->userGroup->name}}</td>
-                                    <td>{{$user->userProfile->lastname}}, {{$user->userProfile->firstname}} {{$user->userProfile->middlename}}. </td>
-                                    <td>{{$user->email}} </td>
-                                    <td>
-                                        <button class="btn btn-success btn-flat open-modal"><i class="glyphicon glyphicon-search"></i></button>
-                                        <button class="btn btn-info btn-flat open-modal-edit" value="{{$user->id}}"><i class="fa fa-pencil-square-o"></i></button>
-                                        <button class="btn btn-danger btn-flat dialog-show-button deleteModal" value="{{$user->id}}" data-show-dialog="my-confirm-dialog"><i class="fa fa-trash-o"></i></button>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                @foreach($users as $user)
+                                    <tr id="user-{{$user->id}}">
+                                        <td>{{$user->id}}</td>
+                                        <td>{{$user->userGroup->name}}</td>
+                                        <td>{{$user->userProfile->lastname}}, {{$user->userProfile->firstname}} {{$user->userProfile->middlename}}. </td>
+                                        <td>{{$user->email}} </td>
+                                        <td>
+                                            <button class="btn btn-success btn-flat open-modal-info" value="{{$user->id}}"><i class="glyphicon glyphicon-search"></i></button>
+                                            <button class="btn btn-info btn-flat open-modal-edit" value="{{$user->id}}"><i class="fa fa-pencil-square-o"></i></button>
+                                            <button type="button" class="btn btn-danger btn-flat deleteModal" data-toggle="modal" data-target=".bs-example-modal-sm" value="{{$user->id}}"><i class="fa fa-trash-o"></i></button>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             @endif
                             </tbody>
                             <tfoot>
                             <tr>
                                 <th>Id</th>
                                 <th>Role</th>
-                                <th>Name</th>
                                 <th>Email</th>
                                 <th>Action</th>
                             </tr>
                             </tfoot>
                         </table>
-                        <div id="my-confirm-dialog" class="dialog-overlay">
-                            <div class="dialog-card">
-                                <div class="dialog-question-sign"><i class="fa fa-question"></i></div>
-                                <div class="dialog-info">
-                                    <h5>Are you sure you want to delete this?</h5>
-                                    <button class="dialog-confirm-button delete-item" id="deleteUser" value="{{$item->id}}">Yes</button>
-                                    <button class="dialog-reject-button">No</button>
+                        <!-- Co                                <th>Name</th>
+nfirmation modal -->
+                        <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" id="confirmBox">
+                            <div class="modal-dialog modal-sm">
+                                <div class="modal-content">
+                                    <h4 class="modal-header" id="gridSystemModalLabel">Are you sure you want to delete?</h4>
+                                    <div class="modal-body">
+                                        <button class="btn btn-danger btn-flat delete-item" id="deleteUser" value="">Yes</button>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </div><!-- end confirmation-->
                     </div><!-- /.box-body -->
                 </div><!-- /.box -->
             </div><!-- /.col -->
