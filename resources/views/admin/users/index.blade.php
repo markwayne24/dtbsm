e06705513d2d581648285bbcbaa2b64882079c4a
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-19 10:19:49 +0800

    Updated index on users views

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -122,131 +122,30 @@
         });
     </script>
     <script>
-        $('document').ready(function() {
-
-            // Prepare reset.
-            function resetModalFormErrors() {
-                $('.form-group').removeClass('has-error');
-                $('.form-group').find('.help-block').remove();
-            }
-
-            // Intercept submit.
-            $('form.bootstrap-modal-form').on('button', function(submission) {
-                submission.preventDefault();
-
-                // Set vars.
-                var form   = $(this),
-                        url    = form.attr('action'),
-                        submit = form.find('[type=button]');
-
-                // Check for file inputs.
-                if (form.find('[type=file]').length) {
-
-                    // If found, prepare submission via FormData object.
-                    var input       = form.serializeArray(),
-                            data        = new FormData(),
-                            contentType = false;
-
-                    // Append input to FormData object.
-                    $.each(input, function(index, input) {
-                        data.append(input.name, input.value);
-                    });
-
-                    // Append files to FormData object.
-                    $.each(form.find('[type=file]'), function(index, input) {
-                        if (input.files.length == 1) {
-                            data.append(input.name, input.files[0]);
-                        } else if (input.files.length > 1) {
-                            data.append(input.name, input.files);
-                        }
-                    });
-                }
-
-                // If no file input found, do not use FormData object (better browser compatibility).
-                else {
-                    var data        = form.serialize(),
-                            contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-                }
-
-                // Please wait.
-                if (submit.is('button')) {
-                    var submitOriginal = submit.html();
-                    submit.html('Please wait...');
-                } else if (submit.is('input')) {
-                    var submitOriginal = submit.val();
-                    submit.val('Please wait...');
-                }
-
-                // Request.
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: data,
-                    dataType: 'json',
-                    cache: false,
-                    contentType: contentType,
-                    processData: false
-
-                    // Response.
-                }).always(function(response, status) {
-
-                    // Reset errors.
-                    resetModalFormErrors();
-
-                    // Check for errors.
-                    if (response.status == 422) {
-                        var errors = $.parseJSON(response.responseText);
-
-                        // Iterate through errors object.
-                        $.each(errors, function(field, message) {
-                            console.error(field+': '+message);
-                            var formGroup = $('[name='+field+']', form).closest('.form-group');
-                            formGroup.addClass('has-error').append('<p class="help-block">'+message+'</p>');
-                        });
-
-                        // Reset submit.
-                        if (submit.is('button')) {
-                            submit.html(submitOriginal);
-                        } else if (submit.is('input')) {
-                            submit.val(submitOriginal);
-                        }
-
-                        // If successful, reload.
-                    } else {
-                        location.reload();
-                    }
-                });
-            });
-
-            // Reset errors when opening modal.
-            $('.bootstrap-modal-form-open').click(function() {
-                resetModalFormErrors();
-            });
-
-        });
-    </script>
-    <script>
         $(document).ready(function(){
+            // Prepare reset.
             var url = "users";
-            var options;
+
             $('.open-modal-edit').click(function(){
                 var id = $(this).val();
-
                 $.ajax({
                     type: 'GET',
                     url: url + '/' + id + '/edit',
                     success: function (data) {
                         console.log(data);
-                        $('#firstname').val(data.firstname);
-                        $('#middlename').val(data.middlename);
-                        $('#lastname').val(data.lastname);
-                        $('#address').val(data.address);
-                        $('#contact_number').val(data.contact_number);
+                        $('#group_id').val(data.user_group.id);
+                        $('#firstname').val(data.user_profile.firstname);
+                        $('#middlename').val(data.user_profile.middlename);
+                        $('#lastname').val(data.user_profile.lastname);
+                        $('#address').val(data.user_profile.address);
+                        $('#gender').val(data.user_profile.gender);
+                        $('#contact_number').val(data.user_profile.contact_number);
+                        $('#email').val(data.email);
                         $('.btn-save').html('Update');
                         $('.btn-save').val('edit');
-                        $('#item_id').val(data.id);
+                        $('#myModalLabel').html('Update Users');
+                        $('#user_id').val(data.id);
                         $('#myModal').modal('show');
-
                     },
                     error: function (data) {
                         console.log('Error:', data);
@@ -255,15 +154,42 @@
 
             });
 
+            $('.open-modal-info').click(function(){
+               var id = $(this).val();
+                $.ajax({
+                    type: 'GET',
+                    url: url + '/' +id + '/edit',
+                    success: function (data){
+                        console.log(data);
+                        $('#infoUserType').html(data.user_group.name);
+                        $('#infoFirstName').html(data.user_profile.firstname);
+                        $('#infoMiddleName').html(data.user_profile.middlename);
+                        $('#infoLastName').html(data.user_profile.lastname);
+                        $('#infoAddress').html(data.user_profile.address);
+                        $('#infoGender').html(data.user_profile.gender);
+                        $('#infoContactNumber').html(data.user_profile.contact_number);
+                        $('#infoEmail').html(data.email);
+                        $('#myModalLabel').html('User Info');
+                        $('#myModalLabels').modal('show');
+                    },
+                    error: function(data){
+                        console.log('Error:',data);
+                    }
+                });
+
+            });
+
             $('.btn-save').click(function (e) {
                 var formData = {
+                    group_id:$('#group_id').val(),
                     firstname: $('#firstname').val(),
                     middlename: $('#middlename').val(),
                     lastname:$('#lastname').val(),
+                    gender:$('#gender').val(),
                     address:$('#address').val(),
                     contact_number: $('#contact_number').val(),
                     email:$('#email').val(),
-                    password:$('#password'),
+                    password:$('#password').val(),
                 };
 
                 //used to determine the http verb to use [add=POST], [update=PUT]
@@ -292,9 +218,13 @@
                     }
                 });
             });
+
             // Clear form fields in a designated area of a page
             $('body').on('hidden.bs.modal', '.modal', function () {
                 $('.btn-save').html('Create').val('add');
+                $('#myModalLabel').html('Create Users');
+                $('#password').val('');
+                $('#password_confirmation').val('');
                 $(this).find('input[type="text"],input[type="email"],textarea,select').each(function() {
                     if (this.defaultValue != '' || this.value != this.defaultValue) {
                         this.value = this.defaultValue;
@@ -315,8 +245,8 @@
                     beforeSend: function() {
                         parent.animate({'backgroundColor':'#fb6c6c'},300);
                     },
-
                     success: function() {
+                        $('#confirmBox').modal('hide')
                         parent.fadeOut(300,function() {
                             parent.remove();
                         });
@@ -332,77 +262,6 @@
                 $('#deleteUser').val(id);
             });
 
-        });
-    </script>
-    <script>
-        $(document).ready(function() {
-
-            // We have two control functions that show or hide dialogs
-
-            function showDialog(id){
-
-                // Find the dialog and show it
-
-                var dialog = $('#' + id),
-                        card = dialog.find('.dialog-card');
-
-                dialog.fadeIn();
-
-                // Center it on screen
-
-                card.css({
-                    'margin-top' : -card.outerHeight()/2
-                });
-
-            }
-
-            function hideAllDialogs(){
-
-                // Hide all visible dialogs
-                $('.dialog-overlay').fadeOut();
-
-            }
-
-            // Here is how to use these functions
-
-            $('.dialog-confirm-button, .dialog-reject-button').on('click', function () {
-
-                // Hide the dialog when the confirm button is pressed
-                hideAllDialogs();
-
-            });
-
-            $('.dialog-overlay').on('click', function (e) {
-
-                if(e.target == this){
-                    // If the overlay was clicked/touched directly, hide the dialog
-                    hideAllDialogs();
-                }
-
-
-            });
-
-            $(document).keyup(function(e) {
-
-                if (e.keyCode == 27) {
-                    // When escape is pressed, hide all dialogs
-
-                    hideAllDialogs();
-                }
-
-            });
-
-
-            // Here, we are listening for clicks on the "show dialog" buttons,
-            // and showing the correct dialog
-
-            $('.dialog-show-button').on('click', function () {
-
-                // Take the contents of the  "data-show-dialog" attribute
-                var toShow = $(this).data('show-dialog');
-
-                showDialog(toShow);
-            });
         });
     </script>
 @stop
