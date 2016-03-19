a5eff47ba255c4f95b8af0bf2bcb697516803105
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-19 10:25:25 +0800

    Added info on admin users view

---
+++ resources/views/admin/users/info.blade.php
@@ -0,0 +1,66 @@
+<!-- Modal -->
+<div class="modal fade" id="myModalLabels" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form role="form" class="bootstrap-modal-form">
+                <div class="modal-header bg-primary">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Create Users</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>User Type</label>
+                        <label class="form-control" id="infoUserType"></label>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <label for="exampleInputFirstName">First Name</label>
+                                <label class="form-control" id="infoFirstName"></label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <label for="exampleInputMiddleName">Middle Name</label>
+                                <label class="form-control" id="infoMiddleName"></label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <label for="exampleInputLastName">Last Name</label>
+                                <label class="form-control" id="infoLastName"></label>
+                            </div>
+                        </div>
+                    </div><!--end of the row-->
+                    <div class="form-group">
+                        <label for="exampleInputAddress">Address</label>
+                        <label class="form-control" id="infoAddress">
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- select -->
+                            <div class="form-group}">
+                                <label>Gender</label>
+                                <label class="form-control" id="infoGender"></label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="ContactNumber">Contact Number</label>
+                                <label class="form-control" id="infoContactNumber"></label>
+                            </div>
+                        </div>
+                    </div><!--end of row-->
+
+                    <div class="form-group">
+                        <label for="Email">Email</label>
+                        <label class="form-control" id="infoEmail"></label>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
