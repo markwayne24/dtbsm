57593c3607290e52f839aa389f197fb8993ca8da
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-19 10:12:42 +0800

    Updated form on views

--- resources/views/admin/items/inventory/form.blade.php
+++ resources/views/admin/items/inventory/form.blade.php
@@ -2,7 +2,7 @@
 <div class="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <form role="form" method="POST" action="inventory" class="bootstrap-modal-form">
+            <form id="frmInventory" name="frmInventory" class="bootstrap-modal-form">
                 <div class="modal-header bg-primary">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title" id="myModalLabel">Create Items</h4>
@@ -11,9 +11,9 @@
                         {{csrf_field()}}
                     <div class="form-group {{ $errors->has('item_id') ? ' has-error' : '' }}">
                         <label>Choose items</label>
-                        <select class="form-control select2" style="width: 100%;" name="item_id">
+                        <select class="form-control select2" style="width: 100%;" name="item_id" id="item_id">
                             @foreach($items as $item)
-                                <option>{{$item->name}}</option>
+                                <option value="{{$item->id}}">{{$item->name}}</option>
                             @endforeach
                         </select>
                         @if ($errors->has('item_id'))
@@ -24,7 +24,7 @@
                     </div>
                     <div class="form-group {{ $errors->has('sku') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">SKU</label>
-                        <input type="text" class="form-control" placeholder="Enter Name of Items" name="sku">
+                        <input type="text" class="form-control" placeholder="Enter Name of Items" name="sku" id="sku">
                         @if ($errors->has('sku'))
                             <span class="help-block">
                         <strong>{{ $errors->first('sku') }}</strong>
@@ -33,7 +33,7 @@
                     </div>
                     <div class="form-group {{ $errors->has('price') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">Price</label>
-                        <input type="text" class="form-control" placeholder="Enter Price" name="price">
+                        <input type="number" class="form-control" placeholder="Enter Price" name="price" id="price">
                         @if ($errors->has('price'))
                             <span class="help-block">
                             <strong>{{ $errors->first('price') }}</strong>
@@ -42,7 +42,7 @@
                     </div>
                     <div class="form-group {{ $errors->has('stocks') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">Stocks</label>
-                        <input type="text" class="form-control" placeholder="Enter Stocks" name="stocks">
+                        <input type="number" class="form-control" placeholder="Enter Stocks" name="stocks" id="stocks">
                         @if ($errors->has('stocks'))
                             <span class="help-block">
                             <strong>{{ $errors->first('stocks') }}</strong>
@@ -52,7 +52,8 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Create</button>
+                    <button type="button" class="btn btn-primary btn-save" value="add">Create</button>
+                    <input type="hidden" id="inventory_id" name="inventory_id">
                 </div>
             </form>
         </div>
