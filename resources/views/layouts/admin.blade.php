a68856fd9ee38f5fe181bfa866718769fb3c0454
Mark Wayne Pulido <Mark Wayne Pulido>
2016-03-22 11:50:36 +0800

    Updated identify gender defualt on admin views

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -54,7 +54,7 @@
 <div class="wrapper">
     <header class="main-header">
         <!-- Logo -->
-        <a href="here.html" class="logo">
+        <a href="" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>A</b></span>
             <!-- logo for regular state and mobile devices -->
