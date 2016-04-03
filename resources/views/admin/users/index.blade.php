5f051e7f2690970050e388dbc9730c498f58cae0
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-03 11:43:44 +0800

    Updated index on admin users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -18,113 +18,15 @@
         });
     </script>
     <script>
-        $('document').ready(function() {
+        $(document).ready(function(){
+            // Prepare reset.
+            var url = "users";
 
             // Prepare reset.
             function resetModalFormErrors() {
                 $('.form-group').removeClass('has-error');
                 $('.form-group').find('.help-block').remove();
             }
-
-            // Intercept submit.
-            $('form.bootstrap-modal-form').on('submit', function(submission) {
-                submission.preventDefault();
-
-                // Set vars.
-                var form   = $(this),
-                        url    = form.attr('action'),
-                        submit = form.find('[type=submit]');
-
-                // Check for file inputs.
-                if (form.find('[type=file]').length) {
-
-                    // If found, prepare submission via FormData object.
-                    var input       = form.serializeArray(),
-                            data        = new FormData(),
-                            contentType = false;
-
-                    // Append input to FormData object.
-                    $.each(input, function(index, input) {
-                        data.append(input.name, input.value);
-                    });
-
-                    // Append files to FormData object.
-                    $.each(form.find('[type=file]'), function(index, input) {
-                        if (input.files.length == 1) {
-                            data.append(input.name, input.files[0]);
-                        } else if (input.files.length > 1) {
-                            data.append(input.name, input.files);
-                        }
-                    });
-                }
-
-                // If no file input found, do not use FormData object (better browser compatibility).
-                else {
-                    var data        = form.serialize(),
-                            contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-                }
-
-                // Please wait.
-                if (submit.is('button')) {
-                    var submitOriginal = submit.html();
-                    submit.html('Please wait...');
-                } else if (submit.is('input')) {
-                    var submitOriginal = submit.val();
-                    submit.val('Please wait...');
-                }
-
-                // Request.
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: data,
-                    dataType: 'json',
-                    cache: false,
-                    contentType: contentType,
-                    processData: false
-
-                    // Response.
-                }).always(function(response, status) {
-
-                    // Reset errors.
-                    resetModalFormErrors();
-
-                    // Check for errors.
-                    if (response.status == 422) {
-                        var errors = $.parseJSON(response.responseText);
-
-                        // Iterate through errors object.
-                        $.each(errors, function(field, message) {
-                            console.error(field+': '+message);
-                            var formGroup = $('[name='+field+']', form).closest('.form-group');
-                            formGroup.addClass('has-error').append('<p class="help-block">'+message+'</p>');
-                        });
-
-                        // Reset submit.
-                        if (submit.is('button')) {
-                            submit.html(submitOriginal);
-                        } else if (submit.is('input')) {
-                            submit.val(submitOriginal);
-                        }
-
-                        // If successful, reload.
-                    } else {
-                        location.reload();
-                    }
-                });
-            });
-
-            // Reset errors when opening modal.
-            $('.bootstrap-modal-form-open').click(function() {
-                resetModalFormErrors();
-            });
-
-        });
-    </script>
-    <script>
-        $(document).ready(function(){
-            // Prepare reset.
-            var url = "users";
 
             $('.open-modal-edit').click(function(){
                 var id = $(this).val();
@@ -179,12 +81,62 @@
 
             });
 
-            $('.btn-save').click(function (e) {
+            $('form.bootstrap-modal-form').on('submit', function(submission) {
+                submission.preventDefault();
+
+                // Set vars.
+                var form   = $(this),
+                        url    = form.attr('action'),
+                        submit = form.find('[type=submit]');
+
+                // Check for file inputs.
+                if (form.find('[type=file]').length) {
+
+                    // If found, prepare submission via FormData object.
+                    var input       = form.serializeArray(),
+                            data        = new FormData(),
+                            contentType = false;
+
+                    // Append input to FormData object.
+                    $.each(input, function(index, input) {
+                        data.append(input.name, input.value);
+                    });
+
+                    // Append files to FormData object.
+                    $.each(form.find('[type=file]'), function(index, input) {
+                        if (input.files.length == 1) {
+                            data.append(input.name, input.files[0]);
+                        } else if (input.files.length > 1) {
+                            data.append(input.name, input.files);
+                        }
+                    });
+                }
+
+                // If no file input found, do not use FormData object (better browser compatibility).
+                else {
+                    var data        = form.serialize(),
+                            contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
+                }
+
+                // Please wait.
+                if (submit.is('button')) {
+                    var submitOriginal = submit.html();
+                    submit.html('Please wait...');
+                } else if (submit.is('input')) {
+                    var submitOriginal = submit.val();
+                    submit.val('Please wait...');
+                }
+
+            //Uppercase First word
+            var firstname = $('#firstname').val().substring(0, 1).toUpperCase() + $('#firstname').val().substring(1);
+            var middlename = $('#middlename').val().substring(0, 1).toUpperCase() + $('#middlename').val().substring(1);
+            var lastname = $('#lastname').val().substring(0, 1).toUpperCase() + $('#lastname').val().substring(1);
+
                 var formData = {
                     group_id:$('#group_id').val(),
-                    firstname: $('#firstname').val(),
-                    middlename: $('#middlename').val(),
-                    lastname:$('#lastname').val(),
+                    firstname: firstname,
+                    middlename: middlename,
+                    lastname: lastname,
                     gender:$('#gender').val(),
                     address:$('#address').val(),
                     contact_number: $('#contact_number').val(),
@@ -196,7 +148,7 @@
                 var state = $('.btn-save').val();
                 var type = "POST"; //for creating new resource
                 var item_id = $('#user_id').val();
-                var my_url = url;
+                var my_url = "users";
 
                 if (state == "edit"){
                     type = 'PUT';
@@ -230,6 +182,7 @@
                         this.value = this.defaultValue;
                     } else { this.value = ''; }
                 });
+                resetModalFormErrors();
             });
 
             //delete task and remove it from list
