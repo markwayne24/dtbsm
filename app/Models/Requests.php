36801ed64f07a9513cef9f5cee0f0825319a359f
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-03 11:52:54 +0800

    Added Requests on Models

---
+++ app/Models/Requests.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Requests extends Model
+{
+    protected $table = 'requests';
+
+    protected $fillable = ['user_id','approved_at'];
+
+    protected $dates = ['approved_at'];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User','user_id');
+    }
+
+    public function itemRequests(){
+        return $this->hasMany('App\Models\ItemRequests');
+    }
+
+
+}
