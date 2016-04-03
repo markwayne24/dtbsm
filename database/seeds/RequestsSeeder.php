161e9c5653195020882b45f57a4b72c84fce5dd4
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-03 11:55:19 +0800

    Added RequestsSeeder on database seeds

---
+++ database/seeds/RequestsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RequestsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('requests')->truncate();
+
+        $requests = [
+            [
+                'user_id'  => 2,
+                'status' => 'Declined'
+            ]
+        ];
+
+        foreach ($requests as $request) {
+            \App\Models\Requests::create($request);
+        }
+    }
+}
