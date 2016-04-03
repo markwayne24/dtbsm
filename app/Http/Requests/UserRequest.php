60c25ce13e2fbe7a809eb4cfa621cda250d3ab15
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-03 11:52:02 +0800

    Added UserRequest on Requests

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|max:56|unique:users,email,' .$this->getSegmentFromEnd().',id',
+            'password' => 'required',
+            'firstname' => 'required|string|min:2|max:56',
+            'middlename' => 'required|string|min:1|max:56',
+            'lastname' => 'required|string|min:2|max:56',
+            'address' => 'required|min:2',
+            'gender' => 'required',
+            'contact_number' => [ 'regex:/^(09|9)\d{9}$/' ],
+            'image'=>'mimes:jpg,jpeg|max:100',
+        ];
+    }
+
+    private function getSegmentFromEnd($position_from_end = 1) {
+        $segments =$this->segments();
+        return $segments[sizeof($segments) - $position_from_end];
+    }
+}
