f8856eb1708682997b236afd833dc22779dfae53
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-14 05:53:54 +0800

    Updated form for inventory on admin views

--- resources/views/admin/items/inventory/form.blade.php
+++ resources/views/admin/items/inventory/form.blade.php
@@ -13,7 +13,7 @@
                         <label>Choose items</label>
                         <select class="form-control select2" style="width: 100%;" name="item_id" id="item_id">
                             @foreach($items as $item)
-                                <option value="{{$item->id}}">{{$item->name}}</option>
+                                <option value="{{$item->id}}">{{$item->itemTypes->name}} - {{$item->name}}</option>
                             @endforeach
                         </select>
                         @if ($errors->has('item_id'))
@@ -22,18 +22,9 @@
                             </span>
                         @endif
                     </div>
-                    <div class="form-group {{ $errors->has('sku') ? ' has-error' : '' }}">
-                        <label for="exampleInputPassword1">SKU</label>
-                        <input type="text" class="form-control" placeholder="Enter Name of Items" name="sku" id="sku">
-                        @if ($errors->has('sku'))
-                            <span class="help-block">
-                        <strong>{{ $errors->first('sku') }}</strong>
-                    </span>
-                        @endif
-                    </div>
                     <div class="form-group {{ $errors->has('price') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">Price</label>
-                        <input type="number" class="form-control" placeholder="Enter Price" name="price" id="price">
+                        <input type="number" class="form-control" min="1" step="0.25" value="0.00" name="price" id="price">
                         @if ($errors->has('price'))
                             <span class="help-block">
                             <strong>{{ $errors->first('price') }}</strong>
@@ -42,7 +33,7 @@
                     </div>
                     <div class="form-group {{ $errors->has('stocks') ? ' has-error' : '' }}">
                         <label for="exampleInputPassword1">Stocks</label>
-                        <input type="number" class="form-control" placeholder="Enter Stocks" name="stocks" id="stocks">
+                        <input type="number" class="form-control" min="1" step="1.00" value="1" name="stocks" id="stocks">
                         @if ($errors->has('stocks'))
                             <span class="help-block">
                             <strong>{{ $errors->first('stocks') }}</strong>
