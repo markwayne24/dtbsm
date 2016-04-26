707a4e53181690ee07aae75e384ed1d735ab6aa4
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-26 22:29:35 +0800

    Updated background profile on users profile on views

--- resources/views/users/profile/profile.blade.php
+++ resources/views/users/profile/profile.blade.php
@@ -12,7 +12,6 @@
     @include('users.profile.form')
     <!-- Main content -->
     <section class="content">
-
         <div class="row">
             <div class="col-md-3">
 
@@ -36,7 +35,7 @@
                         <h3 class="box-title">About Me</h3>
                     </div><!-- /.box-header -->
                     <div class="box-body">
-                        <strong><i class="fa fa-book margin-r-5"></i>  Education</strong>
+                        <strong><i class="fa fa-book margin-r-5"></i> Education</strong>
                         <p class="text-muted">
                             B.S. in Information Technology
                         </p>
@@ -46,81 +45,14 @@
                         <strong><i class="fa fa-map-marker margin-r-5"></i> Address</strong>
                         <p class="text-muted">{{ Auth::user()->userProfile->address }}</p>
 
-                        <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
                     </div><!-- /.box-body -->
                 </div><!-- /.box -->
             </div><!-- /.col -->
-            <div class="col-md-9">
-                <div class="nav-tabs-custom">
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#activity" data-toggle="tab">Activity</a></li>
-                    </ul>
-                    <div class="tab-content">
-                        <div class="active tab-pane" id="activity">
-                            <!-- Post -->
-                            <div class="post">
-                                <div class="user-block">
-                                    <img class="img-circle img-bordered-sm" src="/assets/dist/img/avatar.png" alt="user image">
-                        <span class='username'>
-                          <a href="#">Jonathan Burke Jr.</a>
-                          <a href='#' class='pull-right btn-box-tool'><i class='fa fa-times'></i></a>
-                        </span>
-                                    <span class='description'>Shared publicly - 7:30 PM today</span>
-                                </div><!-- /.user-block -->
-                                <p>
-                                    Lorem ipsum represents a long-held tradition for designers,
-                                    typographers and the like. Some people hate it and argue for
-                                    its demise, but others ignore the hate as they create awesome
-                                    tools to help create filler text for everyone from bacon lovers
-                                    to Charlie Sheen fans.
-                                </p>
-                                <ul class="list-inline">
-                                    <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
-                                    <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a></li>
-                                    <li class="pull-right"><a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments (5)</a></li>
-                                </ul>
-
-                                <input class="form-control input-sm" type="text" placeholder="Type a comment">
-                            </div><!-- /.post -->
-
-                            <!-- Post -->
-                            <div class="post clearfix">
-                                <div class='user-block'>
-                                    <img class='img-circle img-bordered-sm' src='/assets/dist/img/avatar2.png' alt='user image'>
-                        <span class='username'>
-                          <a href="#">Sarah Ross</a>
-                          <a href='#' class='pull-right btn-box-tool'><i class='fa fa-times'></i></a>
-                        </span>
-                                    <span class='description'>Sent you a message - 3 days ago</span>
-                                </div><!-- /.user-block -->
-                                <p>
-                                    Lorem ipsum represents a long-held tradition for designers,
-                                    typographers and the like. Some people hate it and argue for
-                                    its demise, but others ignore the hate as they create awesome
-                                    tools to help create filler text for everyone from bacon lovers
-                                    to Charlie Sheen fans.
-                                </p>
-
-                                <form class='form-horizontal'>
-                                    <div class='form-group margin-bottom-none'>
-                                        <div class='col-sm-9'>
-                                            <input class="form-control input-sm" placeholder="Response">
-                                        </div>
-                                        <div class='col-sm-3'>
-                                            <button class='btn btn-danger pull-right btn-block btn-sm'>Send</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div><!-- /.post -->
-                        </div><!-- /.nav-tabs-custom -->
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
+            </div>
     </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.0
     </div>
     <strong>Copyright &copy; 2015-2016 <a href="#"></a>.</strong> All rights reserved.
 </footer>
