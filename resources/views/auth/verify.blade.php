3c7d484aa2669cf473125ff9f31dcc540ab16077
Mark Wayne Pulido <Mark Wayne Pulido>
2016-04-26 22:27:46 +0800

    Updated background design on verify on views

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,44 +1,58 @@
 @extends('layouts.app')
 
+@section('style')
+    <style>
+        #bgImages{
+            left: 10%;
+            top: 10%;
+            width: 80%;
+            height: 70%;
+            opacity: .3;
+            position:absolute;
+            background-repeat: no-repeat;
+        }
+    </style>
+@stop
+
 @section('content')
     <div class="container">
         <div class="box-body">
-        <div class="row">
-            <div class="col-sm-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Hi, {{ Auth::user()->firstname }} .Please enter your security code.</div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('login/verify') }}">
-                            {!! csrf_field() !!}
+            <div class="row">
+                <div class="col-sm-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Hi, {{ Auth::user()->firstname }} .Please enter your security code.</div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('login/verify') }}">
+                                {!! csrf_field() !!}
 
-                            <div class="form-group{{ $errors->has('code') ? ' has-error' : '' }}">
-                                <label class="col-md-4 control-label">Enter Code</label>
+                                <div class="form-group{{ $errors->has('code') ? ' has-error' : '' }}">
+                                    <label class="col-md-4 control-label">Enter Code</label>
 
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" name="code">
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" name="code">
 
-                                    @if ($errors->has('code'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('code') }}</strong>
-                                    </span>
-                                    @endif
+                                        @if ($errors->has('code'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('code') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-security"></i>Verify
-                                    </button>
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            <i class="fa fa-btn fa-security"></i>Verify
+                                        </button>
 
-                                    <a class="btn btn-link" href="{{ url('#') }}">Regenerate code?</a>
+                                        <a class="btn btn-link" href="{{ url('#') }}">Regenerate code?</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 @endsection
