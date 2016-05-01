dbe227677bb1bb1e3a439522ef2aa9ce43a0377b
Mark Wayne Pulido <Mark Wayne Pulido>
2016-05-02 05:07:31 +0800

    Updated forgot password on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,52 +31,53 @@
 @stop
 @section('content')
     <div class="container">
-    <div class="login-box">
-        <div class="login-box-body">
-            <div class="login-logo">
-                <a href="" >
-                    <h3 class="login-box-msg"><strong>DepEd Tarlac Budget and Supply Monitoring</strong></h3>
-                </a>
-            </div><!-- /.login-logo -->
+        <div class="login-box">
+            <div class="login-box-body">
+                <div class="login-logo">
+                    <a href="" >
+                        <h3 class="login-box-msg"><strong>DepEd Tarlac Budget and Supply Monitoring</strong></h3>
+                    </a>
+                </div><!-- /.login-logo -->
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                    {!! csrf_field() !!}
+                    <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                        @endif
+                    </div>
 
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                {!! csrf_field() !!}
-                <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                    @endif
-                </div>
+                    <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                        @endif
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <div class="checkbox icheck">
+                                <label>
+                                    <input type="checkbox"> Remember Me
+                                </label>
+                                <label>
+                                    <a href="{{ url('/password/reset') }}">I forgot my password?</a><br>
+                                </label>
+                            </div>
+                        </div><!-- /.col -->
+                        <div class="col-xs-4">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                        </div><!-- /.col -->
+                    </div>
+                </form>
 
-                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input type="password" class="form-control" name="password" placeholder="Password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                    @endif
-                </div>
-                <div class="row">
-                    <div class="col-xs-8">
-                        <div class="checkbox icheck">
-                            <label>
-                                <input type="checkbox"> Remember Me
-                            </label>
-                        </div>
-                    </div><!-- /.col -->
-                    <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                    </div><!-- /.col -->
-                </div>
-            </form>
 
-            <a href="#">I forgot my password</a><br>
-
-        </div><!-- /.login-box-body -->
+            </div><!-- /.login-box-body -->
         </div>
     </div><!-- /.login-box -->
 @stop
