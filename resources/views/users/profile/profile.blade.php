12b051ae7e498f24670b5ba2d3a61793e3c467c5
Mark Wayne Pulido <Mark Wayne Pulido>
2016-05-03 01:39:33 +0800

    Updted profile on users views

--- resources/views/users/profile/profile.blade.php
+++ resources/views/users/profile/profile.blade.php
@@ -1,3 +1,7 @@
+@section('style')
+
+@stop
+
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -5,9 +9,6 @@
         <h1>
             Profile
         </h1>
-        @if(Storage::disk('local')->has(Auth()->user()->first_name . '-' . Auth()->user()->id. '.jpg'))
-            <img src="{{ route('account.image',['file' =>$user->first_name. '-' . $user->id . '.jpg']) }}">
-        @endif
     </section>
     @include('users.profile.form')
     <!-- Main content -->
@@ -18,10 +19,14 @@
                 <!-- Profile Image -->
                 <div class="box box-primary">
                     <div class="box-body box-profile">
-                        @if(Auth::user()->userProfile->gender == "male")
-                            <img class="profile-user-img img-responsive img-circle" src="/assets/dist/img/avatar5.png" alt="User profile picture">
-                        @elseif(Auth::user()->userProfile->gender == "female")
-                            <img class="profile-user-img img-responsive img-circle" src="/assets/dist/img/avatar2.png" alt="User profile picture">
+                        @if(file_exists('uploads/' .  Auth::user()->id . '.jpg'))
+                                <img class="profile-user-img img-responsive img-circle" src="/uploads/{{ Auth::user()->id . '.jpg'}}" alt="User profile picture">
+                        @else
+                            @if(Auth::user()->userProfile->gender == "male")
+                                <img class="profile-user-img img-responsive img-circle" src="/assets/dist/img/avatar5.png" alt="User profile picture">
+                            @elseif(Auth::user()->userProfile->gender == "female")
+                                <img class="profile-user-img img-responsive img-circle" src="/assets/dist/img/avatar2.png" alt="User profile picture">
+                            @endif
                         @endif
                             <h3 class="profile-username text-center">{{ Auth::user()->userProfile->firstname }}</h3>
                         <p class="text-muted text-center"> {{ Auth::user()->userGroup->name }}</p>
@@ -31,6 +36,15 @@
 
                 <!-- About Me Box -->
                 <div class="box box-primary">
+                    <form action="upload" id="upload" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="box-header">Upload image</label>
+                                <input type="file" name="file" multiple><br/>
+                                <input type="submit" id="clickUpload">
+                            </div>
+                        </div>
+                    </form>
                     <div class="box-header with-border">
                         <h3 class="box-title">About Me</h3>
                     </div><!-- /.box-header -->
@@ -49,6 +63,21 @@
                 </div><!-- /.box -->
             </div><!-- /.col -->
             </div>
+
+        {{-- <button class="btn btn-primary btn-send">Send</button>--}}
+        <!--alert message if already exists-->
+        <div class="modal fade bs-example-modal-lg" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+            <div class="modal-dialog modal-lg" id ="modal-dialog" >
+                <div class="modal-content" id="modal-content">
+                    <div class="modal-body" id="modal-body">
+                        <div class="message">
+                            <center><H2>Successfully Updated your picture</H2></center>
+                            <center> <button type="button" class="btn btn-default" data-dismiss="modal">Ok</button></center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
 <footer class="main-footer">
