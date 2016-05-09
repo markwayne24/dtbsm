927802debd6ceaec7ade7a419cccbb3220da0dfb
Mark Wayne Pulido <Mark Wayne Pulido>
2016-05-09 16:00:12 +0800

    Updated and added dummy users on UserProfile on seeder

--- database/seeds/UserProfileSeeder.php
+++ database/seeds/UserProfileSeeder.php
@@ -24,6 +24,17 @@ class UserProfileSeeder extends Seeder
                 'gender' => 'male',
                 'contact_number' => '09167563096',
             ],
+            [
+                'user_id'  => 2,
+                'firstname' => 'User',
+                'middlename' => 'U',
+                'lastname' => 'User',
+                'address' => 'Tarlac',
+                'gender' => 'male',
+                'contact_number' => '09124092030',
+                'school'=> 'Buhilit',
+                'district'=> 2
+            ],
         ];
 
         foreach ($userProfiles as $userProfile) {
